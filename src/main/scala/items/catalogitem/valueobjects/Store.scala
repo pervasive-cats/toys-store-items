be667263a895a786e7b31fb7e4e1882e532dcece
turbo-ismam <turboismam@gmail.com>
2023-01-27 00:26:38 +0100

    feat: add store implementation and test

--- src/main/scala/items/catalogitem/valueobjects/Store.scala
+++ src/main/scala/items/catalogitem/valueobjects/Store.scala
@@ -7,9 +7,26 @@
 package io.github.pervasivecats
 package items.catalogitem.valueobjects
 
-import items.Id
+import eu.timepit.refined.api.RefType.applyRef
+
+import items.{Id, Validated, ValidationError}
 
 trait Store {
 
   val id: Id
+}
+
+object Store {
+
+  final private case class StoreImpl(id: Id) extends Store
+
+  case object WrongStoreFormat extends ValidationError {
+
+    override val message: String = "The Store id is a negative value"
+  }
+
+  def apply(value: Long): Validated[Store] = applyRef[Id](value) match {
+    case Left(_) => Left[ValidationError, Store](WrongStoreFormat)
+    case Right(value) => Right[ValidationError, Store](StoreImpl(value))
+  }
 }
---
+++ src/test/scala/items/catalogitem/StoreTest.scala
@@ -0,0 +1,38 @@
+package io.github.pervasivecats
+package items.catalogitem
+
+import io.github.pervasivecats.items.ValidationError
+import io.github.pervasivecats.items.catalogitem.valueobjects.Store
+import io.github.pervasivecats.items.catalogitem.valueobjects.Store.WrongStoreFormat
+
+import eu.timepit.refined.auto.given
+import org.scalatest.EitherValues.given
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.shouldBe
+
+class StoreTest extends AnyFunSpec {
+
+  val negativeValue: Long = -9000
+  val positiveValue: Long = 9000
+  val zeroValue: Long = 0
+
+  describe("A store") {
+    describe("when created with a negative value identifier") {
+      it("should not be valid") {
+        Store(negativeValue) shouldBe Left[ValidationError, Store](WrongStoreFormat)
+      }
+    }
+
+    describe("when created with a positive value identifier") {
+      it("should be valid") {
+        (Store(positiveValue).getOrElse(fail()).id: Long) shouldBe positiveValue
+      }
+    }
+
+    describe("when created with an identifier of value 0") {
+      it("should be valid") {
+        (Store(zeroValue).value.id: Long) shouldBe zeroValue
+      }
+    }
+  }
+}
