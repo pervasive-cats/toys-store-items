be06c656e700b6fefb8daad1fe84fd8556a996a2
turbo-ismam <turboismam@gmail.com>
2023-01-28 13:05:25 +0100

    refactor: scalafmt

--- src/test/scala/items/catalogitem/CatalogItemTest.scala
+++ src/test/scala/items/catalogitem/CatalogItemTest.scala
@@ -1,14 +1,21 @@
 package io.github.pervasivecats
 package items.catalogitem
 
-import io.github.pervasivecats.items.catalogitem.entities.{CatalogItem, InPlaceCatalogItem, LiftedCatalogItem}
-import io.github.pervasivecats.items.catalogitem.valueobjects.{Amount, CatalogItemId, Currency, Price, Store}
-import io.github.pervasivecats.items.itemcategory.valueobjects.ItemCategoryId
-import org.scalatest.funspec.AnyFunSpec
+import io.github.pervasivecats.items.catalogitem.entities.CatalogItem
 import io.github.pervasivecats.items.catalogitem.entities.CatalogItemOps.updated
-import org.scalatest.matchers.should.Matchers.shouldBe
+import io.github.pervasivecats.items.catalogitem.entities.InPlaceCatalogItem
 import io.github.pervasivecats.items.catalogitem.entities.InPlaceCatalogItemOps.lift
+import io.github.pervasivecats.items.catalogitem.entities.LiftedCatalogItem
 import io.github.pervasivecats.items.catalogitem.entities.LiftedCatalogItemOps.putInPlace
+import io.github.pervasivecats.items.catalogitem.valueobjects.Amount
+import io.github.pervasivecats.items.catalogitem.valueobjects.CatalogItemId
+import io.github.pervasivecats.items.catalogitem.valueobjects.Currency
+import io.github.pervasivecats.items.catalogitem.valueobjects.Price
+import io.github.pervasivecats.items.catalogitem.valueobjects.Store
+import io.github.pervasivecats.items.itemcategory.valueobjects.ItemCategoryId
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.shouldBe
 
 class CatalogItemTest extends AnyFunSpec {
 
