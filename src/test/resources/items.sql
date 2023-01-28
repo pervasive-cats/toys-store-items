3db703e5bc5a0a5537ffe2b8f52e9e81e0e503b1
turbo-ismam <turboismam@gmail.com>
2023-01-28 16:48:49 +0100

    feat: added catalog item sql schema

--- src/test/resources/items.sql
+++ src/test/resources/items.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS public.item_categories
     description character varying(100) NOT NULL,
     CONSTRAINT item_categories_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS public.catalog_items
+(
+    id serial NOT NULL DEFAULT,
+    category integer NOT NULL,
+    store integer NOT NULL,
+    price money NOT NULL,
+    CONSTRAINT catalog_items_pkey PRIMARY KEY (id)
+);
