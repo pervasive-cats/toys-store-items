d89bc2874a74e83df2e7d50ae275c0d397245bf8
turbo-ismam <turboismam@gmail.com>
2023-01-28 13:15:00 +0100

    feat: setup Repositories containers implementation and test

--- src/main/scala/items/catalogitem/Repository.scala
+++ src/main/scala/items/catalogitem/Repository.scala
@@ -7,19 +7,46 @@
 package io.github.pervasivecats
 package items.catalogitem
 
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigValueFactory
+import io.getquill.PostgresJdbcContext
+import io.getquill.SnakeCase
+
 import items.Validated
 import items.catalogitem.entities.{CatalogItem, LiftedCatalogItem}
 import items.catalogitem.valueobjects.{CatalogItemId, Price, Store}
 
 trait Repository {
 
-  def findById(catalogItemId: CatalogItemId, store: Store): Validated[CatalogItem]
+  // def findById(catalogItemId: CatalogItemId, store: Store): Validated[CatalogItem]
 
-  def findAllLifted(): Validated[Set[LiftedCatalogItem]]
+  // def findAllLifted(): Validated[Set[LiftedCatalogItem]]
 
-  def add(catalogItem: CatalogItem): Validated[Unit]
+  // def add(catalogItem: CatalogItem): Validated[Unit]
 
-  def update(catalogItem: CatalogItem, price: Price): Validated[Unit]
+  // def update(catalogItem: CatalogItem, price: Price): Validated[Unit]
 
-  def remove(catalogItem: CatalogItem): Validated[Unit]
+  // def remove(catalogItem: CatalogItem): Validated[Unit]
+}
+
+object Repository {
+
+  private class PostgresRepository(ctx: PostgresJdbcContext[SnakeCase]) extends Repository {
+
+    import ctx.*
+
+  }
+
+  def apply: Repository = PostgresRepository(PostgresJdbcContext[SnakeCase](SnakeCase, "ctx"))
+
+  def withPort(port: Int): Repository =
+    PostgresRepository(
+      PostgresJdbcContext[SnakeCase](
+        SnakeCase,
+        ConfigFactory
+          .load()
+          .getConfig("ctx")
+          .withValue("dataSource.portNumber", ConfigValueFactory.fromAnyRef(port))
+      )
+    )
 }
---
+++ src/test/scala/items/catalogitem/RepositoryTest.scala
@@ -0,0 +1,32 @@
+package io.github.pervasivecats
+package items.catalogitem
+
+import scala.concurrent.duration.FiniteDuration
+
+import com.dimafeng.testcontainers.JdbcDatabaseContainer.CommonParams
+import com.dimafeng.testcontainers.PostgreSQLContainer
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import io.getquill.autoQuote
+import org.scalatest.funspec.AnyFunSpec
+import org.testcontainers.utility.DockerImageName
+
+import concurrent.duration.DurationInt
+
+class RepositoryTest extends AnyFunSpec with TestContainerForAll {
+
+  private val timeout: FiniteDuration = 300.seconds
+
+  override val containerDef: PostgreSQLContainer.Def = PostgreSQLContainer.Def(
+    dockerImageName = DockerImageName.parse("postgres:15.1"),
+    databaseName = "users",
+    username = "test",
+    password = "test",
+    commonJdbcParams = CommonParams(timeout, timeout, Some("users.sql"))
+  )
+
+  @SuppressWarnings(Array("org.wartremover.warts.Var", "scalafix:DisableSyntax.var"))
+  private var repository: Option[Repository] = None
+
+  override def afterContainersStart(containers: Containers): Unit =
+    repository = Some(Repository.withPort(containers.container.getFirstMappedPort.intValue()))
+}
