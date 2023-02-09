4f37220405fcfcc25c67728ec39a384122b7c5c6
turbo-ismam <turboismam@gmail.com>
2023-02-09 22:13:53 +0100

    feat: add item state handlers implementation and test

--- src/main/scala/items/item/services/ItemStateHandlers.scala
+++ src/main/scala/items/item/services/ItemStateHandlers.scala
@@ -7,13 +7,38 @@
 package io.github.pervasivecats
 package items.item.services
 
+import io.github.pervasivecats.items.item.entities.{InCartItem, InPlaceItem, ReturnedItem}
+import items.item.Repository as ItemRepository
+import items.catalogitem.Repository as CatalogItemRepository
+import items.catalogitem.entities.{CatalogItem, InPlaceCatalogItem}
 import items.item.domainevents.{ItemAddedToCart, ItemPutInPlace, ItemReturned}
 
 trait ItemStateHandlers {
 
-  def onItemAddedToCart(event: ItemAddedToCart): Unit
+  def onItemAddedToCart(event: ItemAddedToCart)(using ItemRepository, CatalogItemRepository): Unit
 
-  def onItemReturned(event: ItemReturned): Unit
+  def onItemReturned(event: ItemReturned)(using ItemRepository, CatalogItemRepository): Unit
 
-  def onItemPutInPlace(event: ItemPutInPlace): Unit
+  def onItemPutInPlace(event: ItemPutInPlace)(using ItemRepository,CatalogItemRepository): Unit
+}
+
+object ItemStateHandlers extends ItemStateHandlers {
+
+  override def onItemAddedToCart(event: ItemAddedToCart)(using ItemRepository,CatalogItemRepository): Unit = {
+    summon[CatalogItemRepository]
+      .findById(event.catalogItemId, event.store)
+      .map(catalogItem => summon[ItemRepository].update(InCartItem(event.itemId, catalogItem, event.customer)))
+  }
+
+  override def onItemReturned(event: ItemReturned)(using ItemRepository, CatalogItemRepository): Unit = {
+    summon[CatalogItemRepository]
+      .findById(event.catalogItemId, event.store)
+      .map(catalogItem => summon[ItemRepository].update(ReturnedItem(event.itemId, catalogItem)))
+  }
+
+  override def onItemPutInPlace(event: ItemPutInPlace)(using ItemRepository, CatalogItemRepository): Unit = {
+    summon[CatalogItemRepository]
+      .findById(event.catalogItemId, event.store)
+      .map(catalogItem => summon[ItemRepository].update(InPlaceItem(event.itemId, catalogItem)))
+  }
 }
---
+++ src/test/scala/items/item/services/ItemStateHandlersTest.scala
@@ -0,0 +1,113 @@
+package io.github.pervasivecats
+package items.item.services
+
+import com.dimafeng.testcontainers.JdbcDatabaseContainer.CommonParams
+import com.dimafeng.testcontainers.PostgreSQLContainer
+import com.dimafeng.testcontainers.scalatest.{TestContainerForAll, TestContainersForAll}
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigValueFactory
+import eu.timepit.refined.auto.given
+import org.scalatest.EitherValues.given
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.*
+import org.testcontainers.utility.DockerImageName
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import org.scalatest.funspec.AnyFunSpec
+import org.testcontainers.utility.DockerImageName
+import items.item.Repository as ItemRepository
+import items.catalogitem.Repository as CatalogItemRepository
+import items.catalogitem.valueobjects.CatalogItemId
+import items.itemcategory.valueobjects.ItemCategoryId
+import items.catalogitem.valueobjects.Store
+import items.item.entities.InPlaceItemOps.putInCart
+import items.item.entities.InCartItemOps.returnToStore
+import items.item.entities.ReturnedItemOps.putInPlace
+import io.github.pervasivecats.items.item.valueobjects.Customer
+import io.github.pervasivecats.items.item.domainevents.ItemAddedToCart
+import io.github.pervasivecats.items.item.domainevents.ItemReturned
+import io.github.pervasivecats.items.item.domainevents.ItemPutInPlace
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import items.catalogitem.valueobjects.{Amount, Currency, Price}
+import items.item.entities.{InCartItem, InPlaceItem, ReturnedItem}
+import items.catalogitem.entities.{CatalogItem, InPlaceCatalogItem}
+import items.item.valueobjects.ItemId
+
+class ItemStateHandlersTest extends AnyFunSpec with TestContainerForAll {
+
+  private val timeout: FiniteDuration = 300.seconds
+
+  override val containerDef: PostgreSQLContainer.Def = PostgreSQLContainer.Def(
+    dockerImageName = DockerImageName.parse("postgres:15.1"),
+    databaseName = "items",
+    username = "test",
+    password = "test",
+    commonJdbcParams = CommonParams(timeout, timeout, Some("items.sql"))
+  )
+
+  @SuppressWarnings(Array("org.wartremover.warts.Var", "scalafix:DisableSyntax.var"))
+  private var itemRepository: Option[ItemRepository] = None
+
+  @SuppressWarnings(Array("org.wartremover.warts.Var", "scalafix:DisableSyntax.var"))
+  private var catalogItemRepository: Option[CatalogItemRepository] = None
+
+  @SuppressWarnings(Array("org.wartremover.warts.Var", "scalafix:DisableSyntax.var"))
+  private var inPlaceItem: Option[InPlaceItem] = None
+
+  override def afterContainersStart(containers: Containers): Unit = {
+    itemRepository = Some(
+      ItemRepository(
+        ConfigFactory
+          .load()
+          .getConfig("ctx")
+          .withValue("dataSource.portNumber", ConfigValueFactory.fromAnyRef(containers.container.getFirstMappedPort.intValue()))
+      )
+    )
+    catalogItemRepository = Some(
+      CatalogItemRepository(
+        ConfigFactory
+          .load()
+          .getConfig("ctx")
+          .withValue("dataSource.portNumber", ConfigValueFactory.fromAnyRef(containers.container.getFirstMappedPort.intValue()))
+      )
+    )
+    val itemCategoryId: ItemCategoryId = ItemCategoryId(614).getOrElse(fail())
+    val store: Store = Store(15).getOrElse(fail())
+    val price: Price = Price(Amount(19.99).getOrElse(fail()), Currency.withName("EUR"))
+    val catalogItem: InPlaceCatalogItem = catalogItemRepository.getOrElse(fail()).add(itemCategoryId, store, price).getOrElse(fail())
+    val itemId: ItemId = ItemId(9000).value
+    inPlaceItem = Some(InPlaceItem(itemId, catalogItem))
+    itemRepository.getOrElse(fail()).add(inPlaceItem.getOrElse(fail())).getOrElse(fail())
+  }
+
+  describe("An item state handler"){
+    given givenItemRepository: ItemRepository = itemRepository.getOrElse(fail())
+    given givenCatalogItemRepository: CatalogItemRepository = catalogItemRepository.getOrElse(fail())
+
+    describe("when invoked on item added to cart event"){
+      it("should update the database with the given in cart item") {
+        val customer: Customer = Customer("elena@gmail.com").getOrElse(fail())
+        val inCartItem = inPlaceItem.getOrElse(fail()).putInCart(customer)
+        ItemStateHandlers.onItemAddedToCart(ItemAddedToCart(inCartItem.kind.id, inCartItem.kind.store, inCartItem.id, customer))
+        itemRepository.getOrElse(fail()).findById(inCartItem.id, inCartItem.kind.id, inCartItem.kind.store).value shouldBe inCartItem
+      }
+    }
+
+    describe("when invoked on item returned") {
+      it("should update the database with the given returned item") {
+        val customer: Customer = Customer("elena@gmail.com").getOrElse(fail())
+        val inCartItem = inPlaceItem.getOrElse(fail()).putInCart(customer)
+        val returnedItem = inCartItem.returnToStore
+        ItemStateHandlers.onItemReturned(ItemReturned(returnedItem.kind.id, returnedItem.kind.store, returnedItem.id))
+        itemRepository.getOrElse(fail()).findById(returnedItem.id, returnedItem.kind.id, returnedItem.kind.store).value shouldBe returnedItem
+      }
+    }
+
+    describe("when invoked on item put in place") {
+      it("should update the database with the given in place item") {
+        val item = inPlaceItem.getOrElse(fail())
+        ItemStateHandlers.onItemPutInPlace(ItemPutInPlace(item.kind.id, item.kind.store, item.id))
+        itemRepository.getOrElse(fail()).findById(item.id, item.kind.id, item.kind.store).value shouldBe item
+      }
+    }
+  }
+}
